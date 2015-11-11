06953efda2babb2ceac697b4ad19189492574130
afrxx09 <afrxx09@student.lnu.se>
2015-11-11 22:50:57 +0100

    RED

--- tests/ResponseTest.php
+++ tests/ResponseTest.php
@@ -15,4 +15,13 @@ class ResponseTest extends \PHPUnit_Framework_TestCase
     {
         $response = new Response();
     }
+    
+    /**
+     * @test
+     * @covers  Asd\Response::__construct
+     */
+    public function constructor_takesStringArgument()
+    {
+        $response = new Response('string');
+    }
 }
