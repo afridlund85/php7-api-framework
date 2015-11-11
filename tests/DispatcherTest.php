00ac391d62efdf5bd55d8fa0955ef2af098338ef
afrxx09 <afrxx09@student.lnu.se>
2015-11-11 16:07:56 +0100

    REFACTOR: Contradicting tests, fixed it up

--- tests/DispatcherTest.php
+++ tests/DispatcherTest.php
@@ -19,6 +19,28 @@ class DispatcherTest extends \PHPUnit_Framework_TestCase
     
     /**
      * @test
+     * @expectedException   \Exception
+     * @covers              Asd\Dispatcher:__construct
+     */
+    public function constructor_withMissingReponseArgument_throwsException()
+    {
+        $requestStub = $this->getMockBuilder('Asd\iRequest')->getMock();
+        $dispatcher = new Dispatcher($requestStub, null);
+    }
+    
+    /**
+     * @test
+     * @expectedException   \Exception
+     * @covers              Asd\Dispatcher:__construct
+     */
+    public function constructor_withMissingRequestArgument_throwsException()
+    {
+        $responseStub = $this->getMockBuilder('Asd\iResponse')->getMock();
+        $dispatcher = new Dispatcher(null, $responseStub);
+    }
+    
+    /**
+     * @test
      * @covers              Asd\Dispatcher:__construct
      */
     public function constructor_withCorrectArguments_doesNotThrowException()
@@ -28,14 +50,5 @@ class DispatcherTest extends \PHPUnit_Framework_TestCase
         $dispatcher = new Dispatcher($requestStub, $responseStub);
     }
     
-    /**
-     * @test
-     * @expectedException   \Exception
-     * @covers              Asd\Dispatcher:__construct
-     */
-    public function constructor_withMissingReponseArgument_throwsException()
-    {
-        $requestStub = $this->getMockBuilder('Asd\iRequest')->getMock();
-        $dispatcher = new Dispatcher($requestStub, null);
-    }
+    
 }
