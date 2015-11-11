193ff794adf73d8d344c44999a3fa33b40333691
afrxx09 <afrxx09@student.lnu.se>
2015-11-11 23:29:18 +0100

    GREEN

--- src/Response.php
+++ src/Response.php
@@ -4,13 +4,15 @@ namespace Asd;
 
 class Response implements iResponse
 {
+    private $body;
+    
     public function __construct(string $body = '')
     {
-        
+        $this->body = $body;
     }
     
     public function toString() : string
     {
-        
+        return $this->body;
     }
 }
