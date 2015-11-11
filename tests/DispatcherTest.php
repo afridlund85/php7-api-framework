97a88ff85c418a776464416284d0b5bde7209fbe
afrxx09 <afrxx09@student.lnu.se>
2015-11-11 15:05:27 +0100

    REFACTOR: test description typo

--- tests/DispatcherTest.php
+++ tests/DispatcherTest.php
@@ -8,8 +8,8 @@ class DispatcherTest extends \PHPUnit_Framework_TestCase{
     
     /**
     *   @test
-    *   @expectedException \Exception
-    *   @covers \API\Asd:__construct
+    *   @expectedException  \Exception
+    *   @covers             \ASD\Dispatcher:__construct
     */
     public function constructor_withNoArguments_throwsException(){
         $dispatcher = new Dispatcher();
