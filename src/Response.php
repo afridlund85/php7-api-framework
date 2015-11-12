78490b3eebbd9941090330c2e72647d23e0faca6
afrxx09 <afrxx09@student.lnu.se>
2015-11-12 10:16:16 +0100

    REFACTOR: phpdocs for class

--- src/Response.php
+++ src/Response.php
@@ -2,15 +2,27 @@
 
 namespace Asd;
 
+/**
+ * @package Asd
+ */
 class Response implements iResponse
 {
+    /**
+     * @var string
+     */
     private $body;
     
+    /**
+     * @param string $body  Response body
+     */
     public function __construct(string $body = '')
     {
         $this->body = $body;
     }
     
+    /**
+     * @return string   Response body
+     */
     public function toString() : string
     {
         return $this->body;
