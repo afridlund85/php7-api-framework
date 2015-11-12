fb66aca1cc55810be3e3c9178ca65c1fed8d725f
afrxx09 <afrxx09@student.lnu.se>
2015-11-12 12:53:21 +0100

    REFACTOR

--- src/iResponse.php
+++ src/iResponse.php
@@ -6,4 +6,6 @@ interface iResponse
 {
     public function toString() : string;
     public function setBody(string $body);
+    public function getStatusCode() : int;
+    public function setStatusCode(int $statusCode);
 }
