a2dd1ad3a8a13e8b63bd3b92e9b398f53c744d8f
afrxx09 <afrxx09@student.lnu.se>
2015-11-23 12:38:28 +0100

    GREEN

--- src/Request.php
+++ src/Request.php
@@ -60,10 +60,12 @@ class Request implements iRequest{
      */
     private function parseQueries()
     {
-        $this->queries = $_GET;
+        $this->queries = $_GET ?? [];
         if(isset($this->uri) && strpos($this->uri, '?') !== false){
             $queryString = substr($this->uri, strpos($this->uri, '?') + 1);
-            parse_str($queryString, $this->queries);
+            $queries = [];
+            parse_str($queryString, $queries);
+            $this->queries += $queries;
         }
     }
 }
