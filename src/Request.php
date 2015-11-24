89c7d005b79cffb7801a04ab359f4e82aab424ec
afrxx09 <afrxx09@student.lnu.se>
2015-11-24 15:39:40 +0100

    REFACTOR: codecoverage again

--- src/Request.php
+++ src/Request.php
@@ -72,11 +72,11 @@ class Request implements iRequest{
             $uri = explode('?', $this->url);
             $this->uri = $uri[0];
         }
-        if(strpos($this->uri, '/') !== 0){
-            $this->uri = '/' . $this->uri;
-        }
         if(empty($this->uri)){
             $this->uri = '/';
+        }
+        if(strpos($this->uri, '/') !== 0){
+            $this->uri = '/' . $this->uri;
         }
     }
     
